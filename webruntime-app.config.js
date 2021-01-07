1b1791e59780baaf94f254664494df079f4e98ac
Nicolas Robert-Dehault <nrobertdehault@salesforce.com>
2021-01-07 15:53:20 +0100

    GTM custom HTML

--- webruntime-app.config.js
+++ webruntime-app.config.js
@@ -16,10 +16,11 @@ module.exports = {
                     'data.pendo.io', 
                     'app.pendo.io', 
                     'www.googletagmanager.com', 
-                    `'sha256-DYeo5qJWmrIYG0P85Q0alzt+Hp31aNAQdGfIGkVC1PE='`,
-                    `'sha256-SkFuD2DKggQNLr3DLwAyKkIG8j3lKQA5LHOOd/l/XUQ='`,
-                    `'sha256-a7tRDcNFroc5GyK3fKcaTHtiwQJ6G0SRaidkh6Pzlp8='`, 
-                    `'sha256-Y/YmipWDug0DNIzJsB+uTdkIleBMF1A5d9QQfxY4i9M='`
+                    `'sha256-DYeo5qJWmrIYG0P85Q0alzt+Hp31aNAQdGfIGkVC1PE='`, // pendo
+                    `'sha256-SkFuD2DKggQNLr3DLwAyKkIG8j3lKQA5LHOOd/l/XUQ='`, // GTM
+                    `'sha256-a7tRDcNFroc5GyK3fKcaTHtiwQJ6G0SRaidkh6Pzlp8='`, // GA
+                    `'sha256-Y/YmipWDug0DNIzJsB+uTdkIleBMF1A5d9QQfxY4i9M='`, // inline x-oasis-script
+                    `'sha256-dfKUBWBrmo7wXsEj4fQUzFD9wkueKdh3y4CVKFm7zaw='` // GTM custom HTML inline script
                 ]
             },
             loose: true,
